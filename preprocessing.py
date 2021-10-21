8d6685e638c3da91f8fdb26f5eebb7463739be08
aibba19 <andrius96@hotmail.it>
2021-10-21 16:38:20 +0200

    first try preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,58 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+import nltk 
+import re 
+
+
+# With extract corpus we can take the html of the page for retrieving the body of the reviews
+def remove_tags(df):
+    
+    df.replace('((?s)<code>.+?</code>)','',regex= True, inplace= True)
+    df.replace('((?s)<a href=.+?>.+?</a>)','',regex= True, inplace = True)
+    #df.replace('(http.+?</a>)', '' , regex= True, inplace = True ) 
+    df.replace('(<.*?>)','',regex=True, inplace = True)
+    df.replace('(\.?)','',regex=True, inplace = True )
+    print(df)
+    #df.apply(lambda text: BeautifulSoup(text, 'html.parser').get_text())
+    #for index, value in df.items():
+    #    value = re.sub('<.*?>', '', value)
+    #    print(value)
+
+    return df
+
+
+#nltk.download('punkt')
+#nltk.download('stopwords')
+#nltk.download('wordnet')
+
+def clear_text(txt):
+    # Tokenization
+    tokens = word_tokenize(txt)
+
+    # Lowercase conversion
+    tokens = [w.lower() for w in tokens]
+
+    # Removing punctuation
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+
+    # Deleting all non-words
+    final_wds = [w for w in stripped if w.isalpha()]
+
+    # removing stopwords
+    stop_wd = set(stopwords.words('english'))
+    final_wds = [w for w in final_wds if w not in stop_wd]
+
+    # Lemmatization process
+    lemtz = WordNetLemmatizer()
+    final_wds = [lemtz.lemmatize(w) for w in final_wds]
+    final_text = []
+
+    for term in final_wds:
+        final_text.append(term + " ")
+
+    last = ''.join(map(str, final_text))
+
+    return last
