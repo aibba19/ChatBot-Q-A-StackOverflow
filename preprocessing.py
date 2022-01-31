0de3757c8edfbd27f2887adad580aedece427cbb
aibba19 <andrius96@hotmail.it>
2022-01-31 18:24:23 +0100

    add embeddings for ontology data

--- preprocessing.py
+++ preprocessing.py
@@ -45,8 +45,30 @@ def remove_tags(df):
     df.replace("(/\b($def)\b/i)",'',regex=True,inplace=True)
     return df
 
-
-
+def clean_ontology(df):
+   
+    df.loc[:,'function'].replace('(http://rdf.webofcode.org/woc/)','',regex= True, inplace= True)
+    #df.replace('((?s)<code>.+?</code>)','',regex= True, inplace= True)
+    df.replace('(\n)',' ',regex=True,inplace=True)
+    #df.replace('<p>',' ',regex=True,inplace=True)
+    #Delete everything between < > brackets
+    df.replace('(<(.*?)\>)','',regex= True, inplace= True)
+    #df.replace('(</code>)','',regex= True, inplace= True)
+    
+    #df.replace('((?s)@param.*)')
+    #Insert space between lowercase and uppercase character to divide function names as : getAccessibleToken    
+    df.loc[:,'description'].replace(r'(([a-z])([A-Z]))',r"\2 \1",regex = True, inplace = True )
+    
+    #Elimina tutto quello dopo @ nella descrizione della funzione, visto che rappresenta tipo di ritorno e descrizione parametri che non ci interessano  
+    df.loc[:,'description'].replace('(@.*)','',regex= True, inplace= True)
+    
+    #Elimina la descrizione dove è presente una { perchè dai precedenti passi rimangono linee non utili che presentano la parantesi 
+    df.loc[:,'description'].replace('((?s).*{.*)','',regex= True, inplace= True)
+    
+    
+    #df.loc[:,'description'].replace([r"(\w)([A-Z])g"], r'ciao',regex = True, inplace = True )
+    #df.loc[:,'description'].replace("MBeanAttributeInfo", "merda",regex = True, inplace = True )
+    #df.loc[:,'description'] = re.sub(r"([a-z])([A-Z])", r"\g<1> \g<2>", df.loc[:,'description'])
 
 def clear_text(txt, mode):
     
@@ -80,3 +102,4 @@ def clear_text(txt, mode):
     last = ''.join(map(str, final_text))
 
     return last
+
